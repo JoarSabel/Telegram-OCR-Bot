3f2420c51a77a78b6f93afb3c6d510cf51bd89e8
Joar <joar.sabel@netikka.fi>
2023-02-12 15:43:22 +0200

    Clean up the code and convert to better format

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-from asyncio.queues import Queue
 import logging
 from typing import NoReturn
 from PIL import Image
 from pytesseract import pytesseract
-from telegram import Bot, Message
-import asyncio
+from telegram import Bot,Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 import os
 
 def processImage(image_path: str) -> str:
@@ -45,9 +44,6 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
 
 from telegram import Bot
 
-from telegram.error import Forbidden, NetworkError
-
-
 logging.basicConfig(
 
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -57,58 +53,22 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+# When /start is done
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Hello, I am a bot that turns images of text, into text. Use /help for more info!")
 
-async def main() -> NoReturn:
+# When /help is called
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Just send me a screenshot of text with the 'convert' as a caption, that's all I do")
 
-    """Run the bot."""
-
-    # Here we use the `async with` syntax to properly initialize and shutdown resources.
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    async with Bot(BOT_TOKEN) as bot:
-
-        # get the first pending update_id, this is so we can skip over it in case
-
-        # we get a "Forbidden" exception.
-
-        try:
-
-            update_id = (await bot.get_updates())[0].update_id
-
-        except IndexError as e:
-            logger.info('Got IndexError with the following message %s', e)
-            update_id = None
-
-
-        logger.info("listening for new messages... %s", update_id)
-
-        while True:
-
-            try:
-
-                update_id = await handleImage(bot, update_id)
-                #print('bob')
-
-            except NetworkError:
-
-                await asyncio.sleep(1)
-
-            except Forbidden:
-
-                # The user has removed or blocked the bot.
-                if update_id:
-                    update_id = update_id + 1
-
-async def handleImage(bot: Bot, update_id: int) -> int:
-    updates = await bot.get_updates(offset=update_id, timeout=10)
-    for update in updates:
-        next_update_id = update.update_id + 1
-
-
+# When image is sent with 'convert' as caption
+async def convert_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        logger.info('We in the convert now!')
         if update.message:
             if update.message.document and update.message.document.mime_type.__eq__('image/png'):
                 logger.info('Handling image...')
                 file_id = update.message.document.file_id
-                new_file = await bot.get_file(file_id)
+                new_file = await context.bot.get_file(file_id)
                 await new_file.download_to_drive(custom_path='assets/image_to_process.png')
             
                 result = processImage(r'assets/image_to_process.png')
@@ -116,12 +76,28 @@ async def handleImage(bot: Bot, update_id: int) -> int:
                 await update.message.reply_text(result)
             else: 
                 await update.message.reply_text('beep boop, I am a bot designed for OCR tasks only')
-        return next_update_id
-    return update_id
+
+# When someone inputs some random stuff
+async def unkown(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('AAAAAUGH I am a bot designed for OCR tasks ONLY (/help for help)')
+
+def main() -> None:
+
+    """Run the bot."""
+
+    # Here we use the `async with` syntax to properly initialize and shutdown resources.
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+    application = Application.builder().bot(Bot(BOT_TOKEN)).build()
+
+    application.add_handler(CommandHandler("start", start_command))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(MessageHandler(filters.Document.MimeType('image/png') & filters.CaptionRegex(r'convert'),convert_command))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, unkown))
+
+    application.run_polling()
             
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        pass
+    main()
+
